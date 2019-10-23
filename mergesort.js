1f674754532d9b10869a87f73cb1665015d32303
Charlie <achanont@gmail.com>
2019-10-23 15:45:03 -0500

    pass all test specs

--- mergesort.js
+++ mergesort.js
@@ -1,25 +1,47 @@
 const split = arr => {
-  const arr1 = arr.slice(0, Math.floor(arr.length / 2))
-  const arr2 = arr.slice(Math.floor(arr.length / 2))
+  const arr1 = arr.slice(0, Math.floor(arr.length / 2));
+  const arr2 = arr.slice(Math.floor(arr.length / 2));
   return [arr1, arr2];
-}
-
+};
 
 const merge = (arr1, arr2) => {
   const returnedArr = [];
 
   while (arr1.length || arr2.length) {
     if (arr1[0] === undefined) {
-      returnedArr.push(arr2.splice(0, 1)[0])
-    }
-    else if (arr2[0] === undefined) {
-      returnedArr.push(arr1.splice(0, 1)[0])
-    }
-    else {
-      const valToPush = arr1[0] < arr2[0] ? arr1.splice(0, 1)[0] : arr2.splice(0, 1)[0];
-      returnedArr.push(valToPush)
+      returnedArr.push(arr2.splice(0, 1)[0]);
+    } else if (arr2[0] === undefined) {
+      returnedArr.push(arr1.splice(0, 1)[0]);
+    } else {
+      const valToPush =
+        arr1[0] < arr2[0] ? arr1.splice(0, 1)[0] : arr2.splice(0, 1)[0];
+      returnedArr.push(valToPush);
     }
   }
 
   return returnedArr;
-}
+};
+
+const mergeSort = array => {
+  const len = array.length;
+  const newArray = split(array);
+  let count = 0;
+
+  while (newArray.length < len) {
+    if (newArray[count].length > 1) {
+      const tempArr = split(newArray[count]);
+      newArray.splice(count, 1);
+      newArray.push(tempArr[1]);
+      newArray.push(tempArr[0]);
+    } else {
+      count++;
+    }
+  }
+
+  while (newArray[1]) {
+    newArray[0] = merge(newArray[0], newArray[1]);
+    newArray.splice(1, 1);
+  }
+
+  return newArray[0];
+};
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,19 +1,17 @@
-
-
 describe('split function', function() {
-  it('should return an array', function () {
-    expect(Array.isArray(split([1,2,3,4]))).toEqual(true);
+  it('should return an array', function() {
+    expect(Array.isArray(split([1, 2, 3, 4]))).toEqual(true);
   });
 
   it('Should return an array of two arrays', function() {
-    const testArr = split([1,2,3,4]);
+    const testArr = split([1, 2, 3, 4]);
     expect(testArr.length).toEqual(2);
     expect(Array.isArray(testArr[0])).toEqual(true);
     expect(Array.isArray(testArr[1])).toEqual(true);
   });
 
   it('Should be able to handle odd-length array', function() {
-    const testArr = split([1,2,3,4,5]);
+    const testArr = split([1, 2, 3, 4, 5]);
     expect(testArr[0].length).toEqual(2);
     expect(testArr[1].length).toEqual(3);
   });
@@ -21,16 +19,16 @@ describe('split function', function() {
 
 describe('merge function', function() {
   it('Should return a single sorted array', function() {
-    expect(merge([1,3], [2,4])).toEqual([1,2,3,4])
+    expect(merge([1, 3], [2, 4])).toEqual([1, 2, 3, 4]);
   });
   it('Should work with odd-length arrays', function() {
-    expect(merge([1,3,5], [2,4])).toEqual([1,2,3,4,5])
+    expect(merge([1, 3, 5], [2, 4])).toEqual([1, 2, 3, 4, 5]);
   });
 });
 
 describe('mergeSort function', function() {
   it('Should return an array', function() {
-    expect(Array.isArray(mergeSort([1,2,3,4]))).toEqual(true);
+    expect(Array.isArray(mergeSort([1, 2, 3, 4]))).toEqual(true);
   });
 
   it('Should sort an array of numbers', function() {
@@ -39,7 +37,7 @@ describe('mergeSort function', function() {
   });
 
   it('Should sort an array of strings', function() {
-    let testArr = ['bcd', 'abc', 'x', 'p', 'daring', ];
+    let testArr = ['bcd', 'abc', 'x', 'p', 'daring'];
     expect(mergeSort(testArr)).toEqual(['abc', 'bcd', 'daring', 'p', 'x']);
   });
 
@@ -47,5 +45,4 @@ describe('mergeSort function', function() {
     let testArr = [-5, -10, 20, 100];
     expect(mergeSort(testArr)).toEqual([-10, -5, 20, 100]);
   });
-
 });
