28399316f5c1cf2d98a37607ec359efa62378b7d
Charlie <achanont@gmail.com>
2019-10-23 11:33:37 -0500

    finished project

--- bubblesort.js
+++ bubblesort.js
@@ -1,18 +1,23 @@
 const swap = (a, b) => {
   return [b, a];
-}
+};
+
+const swapObj = {
+  swap: swap
+};
+
 const bubbleSort = arr => {
   let elsToCheck = arr.length - 1;
-  
+
   while (elsToCheck > 0) {
     for (let i = 0; i < elsToCheck; i++) {
       if (arr[i] > arr[i + 1]) {
-        let tempArr = swap(arr[i], arr[i + 1]);
-        arr[i] = tempArr[0]
-        arr[i + 1] = tempArr[1]
+        let tempArr = swapObj.swap(arr[i], arr[i + 1]);
+        arr[i] = tempArr[0];
+        arr[i + 1] = tempArr[1];
       }
     }
     elsToCheck--;
   }
   return arr;
-}
+};
